e8d1e95cc2a8915ca243307345ab62f429dfda4e
Alex Grimm <alex-grimm@web.de>
2026-03-28 10:56:06 +0100

    fix: correct Colors import path in delete page

--- app/delete.tsx
+++ app/delete.tsx
@@ -8,7 +8,7 @@ import {
   TouchableOpacity,
   Platform,
 } from "react-native";
-import { Colors } from "../constants/Colors";
+import { Colors } from "../constants/colors";
 
 const CONTACT_EMAIL = "kneebackapp@gmail.com";
 
